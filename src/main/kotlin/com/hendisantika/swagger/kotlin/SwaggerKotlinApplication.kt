3068e6c0ff4244c049afdae5db385975d4b789cd
Hendi Santika <hendisantika@gmail.com>
2017-09-13 05:51:24 +0700

    #1 - Initial Commit

---
+++ src/main/kotlin/com/hendisantika/swagger/kotlin/SwaggerKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.hendisantika.swagger.kotlin
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SwaggerKotlinApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(SwaggerKotlinApplication::class.java, *args)
+}
