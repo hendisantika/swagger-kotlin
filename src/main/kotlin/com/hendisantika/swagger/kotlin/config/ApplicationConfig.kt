5440be28a13b4f03ee5baaa44c50030b30ea84e7
Hendi Santika <hendisantika@gmail.com>
2017-09-13 06:05:28 +0700

    #3 - Added Application Config

---
+++ src/main/kotlin/com/hendisantika/swagger/kotlin/config/ApplicationConfig.kt
@@ -0,0 +1,49 @@
+package com.hendisantika.swagger.kotlin.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.Contact
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/13/17
+ * Time: 6:03 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Configuration
+@EnableSwagger2
+class ApplicationConfig {
+    @Bean
+    fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.hendisantika.swagger.kotlin.controllers"))
+                .paths(PathSelectors.any())
+                .build()
+    }
+
+    private fun getApiInfo(): ApiInfo {
+        val contact = Contact("Hendi Santika", "http://hendisantika.wordpress.com", "hendisantika@gmail.com")
+        return ApiInfoBuilder()
+                .title("Example Api Title")
+                .description("Example Api Definition")
+                .version("1.0.0")
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
+                .contact(contact)
+                .build()
+    }
+}
