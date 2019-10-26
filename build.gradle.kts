eaec60b8360259924099928cccd2482713b17180
Hendi Santika <hendisantika@yahoo.co.id>
2019-10-27 05:37:38 +0700

    #20 - Upgraded into Spring Boot 2.1.6.RELEASE version
    #20 - Upgraded into Spring Boot 2.1.6.RELEASE version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     val kotlinVersion = "1.3.11"
     kotlin("jvm") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
-    id("org.springframework.boot") version "2.1.5.RELEASE"
+    id("org.springframework.boot") version "2.1.6.RELEASE"
     id("io.spring.dependency-management") version "1.0.6.RELEASE"
 }
 
