f7c7ee92a9e41cd8803009c01159f5e35a5b9c24
Hendi Santika <hendisantika@yahoo.co.id>
2020-08-31 07:06:03 +0700

    #42 - Upgraded into Spring Boot 2.3.8.RELEASE version
    #42 - Upgraded into Spring Boot 2.3.8.RELEASE version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     val kotlinVersion = "1.3.11"
     kotlin("jvm") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
-    id("org.springframework.boot") version "2.3.7.RELEASE"
+    id("org.springframework.boot") version "2.3.8.RELEASE"
     id("io.spring.dependency-management") version "1.0.6.RELEASE"
 }
 
