c8895e1538200e40b829aa7485fab042460f0375
Hendi Santika <hendisantika@yahoo.co.id>
2021-12-06 05:08:40 +0700

    #59 - Upgraded into Spring Boot 2.5.1 version
    #59 - Upgraded into Spring Boot 2.5.1 version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.5.0"
+    id("org.springframework.boot") version "2.5.1"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
     kotlin("jvm") version "1.4.10"
     kotlin("plugin.spring") version "1.4.10"
