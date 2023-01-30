bd7613ab0de3ed9d7dec26f7155d207179497df1
Hendi Santika <hendisantika@yahoo.co.id>
2023-01-31 06:19:50 +0700

    #108 - Upgraded into Spring Boot 3.0.0 version ✏️ 📗 :octocat:
    #108 - Upgraded into Spring Boot 3.0.0 version ✏️ 📗 :octocat:

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.9.9"
+    id("org.springframework.boot") version "3.0.0"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
     kotlin("jvm") version "1.4.10"
     kotlin("plugin.spring") version "1.4.10"
@@ -9,7 +9,7 @@ plugins {
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
