a84236de81d85da0f55123ab12bd936ec4f49c57
Hendi Santika <hendisantika@yahoo.co.id>
2024-05-13 09:56:11 +0700

    #131 - Upgraded into Spring Boot 3.2.2 version ✏️📗 :octocat: 🐳⬆
    #131 - Upgraded into Spring Boot 3.2.2 version ✏️📗 :octocat: 🐳⬆

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.2.1"
+    id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
     kotlin("jvm") version "1.4.10"
     kotlin("plugin.spring") version "1.4.10"
